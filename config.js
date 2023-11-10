68510a49be30f240cdbb4e17ed1fda2247f1c042
Yellow Records <nathanielhofer81305@icloud.com>
2023-11-09 20:07:14 -0500

    movieweb

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-web.nathanielhofer81305.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzI5MjdhM2ExZDViOWE2OWFmZDYxYmZiZWNlMDQ3ZiIsInN1YiI6IjVmYTcyMjJhMzRlMTUyMDA0MDc5NGMyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FmaE9RNMGqqxoivwVA2xiN5HChazwKcJ6O6uLbMi-nk"
+};
